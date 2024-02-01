6e04658690fa5508f9a8e71dd90f68cccd631ecd
Pesanth Janaseth Rangaswamy Anitha <pesanth10@gmail.com>
2024-01-31 21:10:55 -0400

    Add Sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>Project1</section>
+    <section>Project2</section>
+    <section>Project3</section>
+    <section>Contacts</section>
+  </div>;
 };
 
 export default App;
